fead39e8e7e3418b1be90e4f178fcf05270be35b
Hari994057 <hariharan8076@gmail.com>
2025-08-28 17:43:21 +0530

    Update python_app (1).py

--- python_app (1).py
+++ python_app (1).py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route('/welcome')
 def welcome():
-    return "Welcome to Python! "
+    return "jenkins app updated "
 
 
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=80)
+
 
